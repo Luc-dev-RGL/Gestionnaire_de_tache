e2bf94582d67017a733c77933793b2a0b1653f74
Jean-Luc Kouassi <smith00547986@gmail.com>
2026-09-03 08:49:57 +0000

    Ajout des textes : titre gras 24sp + sous-titre 16sp + espacements

--- app/src/main/java/ci/pigier/gestionnaire_de_tache/MainActivity.kt
+++ app/src/main/java/ci/pigier/gestionnaire_de_tache/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -18,8 +20,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ci.pigier.gestionnaire_de_tache.ui.theme.Gestionnaire_de_tacheTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,6 +55,18 @@ fun TacheCompleteScreen() {
             painter = painterResource(id = R.drawable.ic_task_completed),
             contentDescription = "Tâches terminées",
             modifier = Modifier.size(120.dp)
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
         )
 
     }
