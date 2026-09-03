e2cae362cfdc394cf4006b5403953473392004f4
Jean-Luc Kouassi <smith00547986@gmail.com>
2026-09-03 08:21:10 +0000

    Création de TacheCompleteScreen avec colonne centrée

--- app/src/main/java/ci/pigier/gestionnaire_de_tache/MainActivity.kt
+++ app/src/main/java/ci/pigier/gestionnaire_de_tache/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ci.pigier.gestionnaire_de_tache.ui.theme.Gestionnaire_de_tacheTheme
@@ -19,11 +24,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Gestionnaire_de_tacheTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TacheCompleteScreen()
                 }
             }
         }
@@ -31,17 +36,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TacheCompleteScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(align = Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TacheCompleteScreenPreview() {
     Gestionnaire_de_tacheTheme {
-        Greeting("Android")
+        TacheCompleteScreen()
     }
 }
