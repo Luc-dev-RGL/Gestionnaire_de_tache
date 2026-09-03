b172bd0e22cacde60f586ffae5e1ee06c9425751
Jean-Luc Kouassi <smith00547986@gmail.com>
2026-09-03 08:06:50 +0000

    Mise en place du projet "Projet Initial Android Studio"

---
+++ app/src/main/java/ci/pigier/gestionnaire_de_tache/MainActivity.kt
@@ -0,0 +1,47 @@
+package ci.pigier.gestionnaire_de_tache
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ci.pigier.gestionnaire_de_tache.ui.theme.Gestionnaire_de_tacheTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Gestionnaire_de_tacheTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Gestionnaire_de_tacheTheme {
+        Greeting("Android")
+    }
+}
